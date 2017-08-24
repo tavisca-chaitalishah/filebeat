88aa69b988de953e62f676469a81642d99da0d67
tavisca-chaitalishah <cshah@tavisca.com>
2017-08-24 13:31:47 +0530

    bug fix in kitchen.yml

--- cookbooks/filebeat/test/smoke/default/default_test.rb
+++ cookbooks/filebeat/test/smoke/default/default_test.rb
@@ -4,7 +4,3 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
-describe package('filebeat') do
-	it { should be_installed }
-end
